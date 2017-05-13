fa702b44de12cff062ca6be3c87c397d0fe095e0
Sung J Byun <sungjbyun@Sungs-MacBook-Pro-2.local>
2017-05-12 23:48:59 -0700

    dashboard added

---
+++ src/entry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// import routes from './routes';
+import 'normalize.css';
+
+window.React = React;
+
+render (
+    <h1>HELLO WORLD</h1>,
+    document.getElementById('root')
+)
+
